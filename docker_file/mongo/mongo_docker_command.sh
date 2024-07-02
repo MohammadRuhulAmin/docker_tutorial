f80006e96ca832c19d058e946d8bae205a1f4018
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-07-02 10:58:46 +0600

    mongo run command

---
+++ docker_file/mongo/mongo_docker_command.sh
@@ -0,0 +1,19 @@
+# Running the mongodb server
+docker run -d \
+-p 27017:27017 \
+-e MONGO_INITDB_ROOT_USERNAME=admin \
+-e MONGO_INITDB_ROOT_PASSWORD=password \
+--network mongo-network \
+--name mongodb \
+mongo
+
+# Running the mongo-express server
+docker run -d \
+-p 8081:8081 \
+-e ME_CONFIG_MONGODB_ADMINUSERNAME=admin \
+-e ME_CONFIG_MONGODB_ADMINPASSWORD=password \
+-e ME_CONFIG_MONGODB_SERVER=mongodb \
+--network mongo-network \
+mongo-express
+
+ 
