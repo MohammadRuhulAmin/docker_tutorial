bcf6209523ba8c120f6b6e20660a9c04be59a0f4
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-07-02 14:50:09 +0600

    mysql_docker_command.sh updated

--- docker_file/mysql/mysql_docker_command.sh
+++ docker_file/mysql/mysql_docker_command.sh
@@ -1,7 +1,6 @@
-docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:tag
 
 docker run -d \
---name mysql_cont \
--e MYSQL_ROOT_PASSWORD=my-secrect-pw \
--p 8084:8085 \
+-p 8085:3306 \
+-e MYSQL_ROOT_PASSWORD=ruhul \
+-e MYSQL_DATABASE=testdb \
 mysql 
